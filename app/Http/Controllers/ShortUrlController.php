6af53103fb9da0b18eb90ef809bafe5d9bc3f1fb
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2024-11-27 16:57:04 +0600

    Model, controller and migration file setup

---
+++ app/Http/Controllers/ShortUrlController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\ShortUrl;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+
+final class ShortUrlController
+{
+
+
+}
